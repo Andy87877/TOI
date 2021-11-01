b3fd9ed69d08edadbb40d395d2410cdefd0f2f7b
Andy8787 <86054592+Andy87877@users.noreply.github.com>
2021-11-02 00:55:14 +0800

    Update Noob3.cpp

--- CPP/2021.10/Noob3.cpp
+++ CPP/2021.10/Noob3.cpp
@@ -5,34 +5,42 @@ using namespace std;
 int main() {
     int N, M, D;
     cin >> N >> M >> D;
+    for (int i = 0; i <= D; i += N) { // N可以整除
+        if (((D-i) % M) == 0) { // M可以整除
+            cout << "YES";
+            return 0;
+        }
+    }
+    cout << "NO";
+}
+
+/* 舊版(很容易TLE 雖然送出沒有)
+int main() {
+    int N, M, D;
+    cin >> N >> M >> D;
 
     int temp = 0; // M幾次
-    bool flag = false;// 是否有
 
     while (true) {
         int total = 0; // 總和
         for (int i = 0; i < temp; i++) {
             total += M;
             if (total == D) {
-                flag = true;
                 cout << "YES";
                 return 0;
             } else if (total > D) {
-                flag = false;
                 cout << "NO";
                 return 0;
             }
         }
 
         if (total == D) {
-            flag = true;
             break;
         }
 
         while (true) {
             total += N;
             if (total == D) {
-                flag = true;
                 cout << "YES";
                 return 0;
             } else if (total > D) {
@@ -41,11 +49,5 @@ int main() {
         }
         temp += 1;
     }
-    /*
-    if (flag) {
-        cout << "YES";
-    } else {
-        cout << "NO";
-    }
-    */
 }   
+*/
